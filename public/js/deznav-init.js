818c6a99a20db25583e69b651ef9fb4d98c53223
Eric <eric@loadx.org>
2021-05-07 18:45:43 +0300

    initial fasto commit (folder copy)

---
+++ public/js/deznav-init.js
@@ -0,0 +1,58 @@
+
+var dezSettingsOptions = {};
+
+function getUrlParams(dParam) 
+	{
+		var dPageURL = window.location.search.substring(1),
+			dURLVariables = dPageURL.split('&'),
+			dParameterName,
+			i;
+
+		for (i = 0; i < dURLVariables.length; i++) {
+			dParameterName = dURLVariables[i].split('=');
+
+			if (dParameterName[0] === dParam) {
+				return dParameterName[1] === undefined ? true : decodeURIComponent(dParameterName[1]);
+			}
+		}
+	}
+
+(function($) {
+	
+	"use strict"
+	
+	var direction =  getUrlParams('dir');
+	
+	dezSettingsOptions = {
+		typography: "poppins",
+			version: "dark",
+			layout: "horizontal",
+			headerBg: "color_12",
+			navheaderBg: "color_12",
+			sidebarBg: "color_12",
+			sidebarStyle: "mini",
+			sidebarPosition: "fixed",
+			headerPosition: "static",
+			containerLayout: "wide",
+			direction: direction
+		};
+
+	
+	if(direction == 'rtl')
+	{
+        direction = 'rtl'; 
+    }else{
+        direction = 'ltr'; 
+    }
+	
+	new dezSettings(dezSettingsOptions); 
+
+	jQuery(window).on('resize',function(){
+        /*Check container layout on resize */
+        dezSettingsOptions.containerLayout = $('#container_layout').val();
+        /*Check container layout on resize END */
+        
+		new dezSettings(dezSettingsOptions); 
+	});
+	
+})(jQuery);
